c8692f1fa148c1d3ca0e4f9c6c4683f1fb16de66
guivictorr <guilhermevictor20@gmail.com>
2021-04-16 18:18:30 -0300

    deploy config

--- src/commands/figurinha.ts
+++ src/commands/figurinha.ts
@@ -2,24 +2,22 @@ import { Message } from 'whatsapp-web.js';
 
 const figurinha = async (msg: Message): Promise<Message> => {
   const { id } = await msg.getChat();
+  const validTypes = ['image', 'video'];
 
   if (!msg.hasQuotedMsg) {
     return msg.reply('🤖 Não encontrei imagem');
   }
   const quotedMessage = await msg.getQuotedMessage();
 
-  if (quotedMessage.type !== 'image') {
+  if (!validTypes.includes(quotedMessage.type)) {
     return msg.reply('🤖 Não encontrei imagem');
   }
 
   if (!quotedMessage.hasMedia) {
-    return msg.reply(
-      '🤖 Não consigo baixar esta imagem\n\n❗ Se a imagem não tem o símbolo de download, o bot não consegue baixar a imagem com uma boa qualidade',
-    );
+    return msg.reply('🤖 Não consigo baixar esta imagem');
   }
 
   const media = await quotedMessage.downloadMedia();
-
   return msg.reply(`Ta ai a figurinha`, id._serialized, {
     media,
     sendMediaAsSticker: true,
