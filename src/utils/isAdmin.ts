8211d608aec1ed848b55d37b9e62a7e2a4608499
guivictorr <guilhermevictor20@gmail.com>
2021-05-12 19:00:36 -0300

    fixed isAdmin.ts

--- src/utils/isAdmin.ts
+++ src/utils/isAdmin.ts
@@ -2,7 +2,7 @@ import { ContactId, GroupParticipant } from 'whatsapp-web.js';
 
 const isAdmin = (id: ContactId, members: GroupParticipant[]): boolean => {
   const userIndex = members.findIndex(
-    (participant: GroupParticipant) => participant.id.user === id._serialized,
+    (participant: GroupParticipant) => participant.id.user === id.user,
   );
 
   const isAdmin = members[userIndex].isAdmin;
