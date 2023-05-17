8752f9794b879498049a52aaea015ac7884064af
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-16 23:06:26 -0300

    updating whatsapp-web.js and adding try catch for figurinha

--- src/commands/global/figurinha.ts
+++ src/commands/global/figurinha.ts
@@ -2,25 +2,29 @@ import { Message } from 'whatsapp-web.js';
 import client from '../../server';
 
 const figurinha = async (msg: Message): Promise<Message> => {
-  const { id } = await msg.getChat();
-  const validTypes = ['image', 'video'];
+  try {
+    const { id } = await msg.getChat();
+    const validTypes = ['image', 'video'];
 
-  if (!msg.hasQuotedMsg) {
-    return msg.reply('🤖 Você precisa responder a mensagem com a imagem.');
+    if (!msg.hasQuotedMsg) {
+      return msg.reply('🤖 Você precisa responder a mensagem com a imagem.');
+    }
+
+    const quotedMessage = await msg.getQuotedMessage();
+
+    if (!validTypes.includes(quotedMessage.type)) {
+      return msg.reply('🤖 Não encontrei nenhum vídeo/gif/foto');
+    }
+
+    client.sendMessage(id._serialized, '🤖 Carregando');
+    const media = await quotedMessage.downloadMedia();
+    return msg.reply(`Ta ai a figurinha`, id._serialized, {
+      media,
+      sendMediaAsSticker: true,
+    });
+  } catch (error) {
+    return msg.reply('Error trying to convert to sticker');
   }
-
-  const quotedMessage = await msg.getQuotedMessage();
-
-  if (!validTypes.includes(quotedMessage.type)) {
-    return msg.reply('🤖 Não encontrei nenhum vídeo/gif/foto');
-  }
-
-  client.sendMessage(id._serialized, '🤖 Carregando');
-  const media = await quotedMessage.downloadMedia();
-  return msg.reply(`Ta ai a figurinha`, id._serialized, {
-    media,
-    sendMediaAsSticker: true,
-  });
 };
 
 export default figurinha;
