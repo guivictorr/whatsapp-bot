7867989a50067df147bb73a7dbb943c61df1d603
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-09-15 18:42:32 -0300

    refactor: linting files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { includeIgnoreFile } from "@eslint/compat";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from '@eslint/compat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
